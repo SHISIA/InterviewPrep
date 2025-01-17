b111a0c7fbb1bf70fc57943abf280a2f24089bff
Shisia <jamesshisia6@gmail.com>
2025-01-17 09:29:05 +0300

    Assign/Replace/Reassign array value

--- src/main/java/org/prep/ArraysPrep.java
+++ src/main/java/org/prep/ArraysPrep.java
@@ -1,5 +1,12 @@
 package org.prep;
 
+
+import java.util.Arrays;
+
+/**
+ * @Type: <b>Array:</b>
+ * @Definition: An array is a collection of items of the same object type called Data type
+ * **/
 public class ArraysPrep {
 
 
@@ -7,6 +14,7 @@ public class ArraysPrep {
         String[] cars={"Camaro","Corvette","BMW","Tesla","Fisker","Ford Mustang"};
         ArraysPrep ap=new ArraysPrep();
         ap.printArrayPositions(cars);
+        ap.assignArrayValues(cars);
     }
 
     private void printArrayPositions(String[] arr){
@@ -14,5 +22,13 @@ public class ArraysPrep {
         System.out.println(arr[0]);//first element
         System.out.println(arr[1]);//second element
         System.out.println(arr[arr.length-1]);//last element
+    }
+
+    private void assignArrayValues(String[] arr){
+        arr[0]="Mazda";//sets the first item value to Mazda
+        arr[1]="Mercedes";//sets the second item value to Mercedes
+        arr[2]="Volkswagen";//sets the third array item value to Volkswagen
+        arr[arr.length-1]="Mahindra";//sets the last array item value to Mahindra
+        System.out.println(Arrays.toString(arr));//converts the array values into a printable string
     }
 }
