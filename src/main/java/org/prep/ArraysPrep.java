be6494996b6ceca90b8e5bd69bfd32d43fd9b88b
Shisia <jamesshisia6@gmail.com>
2025-01-17 09:41:03 +0300

    Assign/Replace/Reassign array value

--- src/main/java/org/prep/ArraysPrep.java
+++ src/main/java/org/prep/ArraysPrep.java
@@ -22,6 +22,7 @@ public class ArraysPrep {
         System.out.println(arr[0]);//first element
         System.out.println(arr[1]);//second element
         System.out.println(arr[arr.length-1]);//last element
+        anonymousArray();
     }
 
     private void assignArrayValues(String[] arr){
@@ -30,5 +31,16 @@ public class ArraysPrep {
         arr[2]="Volkswagen";//sets the third array item value to Volkswagen
         arr[arr.length-1]="Mahindra";//sets the last array item value to Mahindra
         System.out.println(Arrays.toString(arr));//converts the array values into a printable string
+    }
+
+    /***
+     * <b>Anonymous Array: </b>An array in Java without any name is known as an anonymous array. It is an array just for creating and using instantly.
+     * Using an anonymous array, we can pass an array with user values without the referenced variable.
+     * <a href="https://www.geeksforgeeks.org/anonymous-array-java/">Click to learn more about anonymous arrays</a>
+     * **/
+    private void anonymousArray(){
+        System.out.println(Arrays.toString(new String[10]));//prints [null,null,null....ten times]
+        System.out.println(Arrays.toString(new String[0]));//prints []
+        System.out.println(Arrays.toString(new String[]{"shisia","James","Peter","Paula"}));//prints [shisia,James,Peter,Paula]
     }
 }
