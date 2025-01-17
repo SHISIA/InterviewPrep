ced10105f54366820bd5811ce02af96fb37ddb1f
Shisia <jamesshisia6@gmail.com>
2025-01-17 09:15:13 +0300

    Understand array positions

--- src/main/java/org/prep/ArraysPrep.java
+++ src/main/java/org/prep/ArraysPrep.java
@@ -1,4 +1,18 @@
 package org.prep;
 
 public class ArraysPrep {
+
+
+    public static void main(String[] args){
+        String[] cars={"Camaro","Corvette","BMW","Tesla","Fisker","Ford Mustang"};
+        ArraysPrep ap=new ArraysPrep();
+        ap.printArrayPositions(cars);
+    }
+
+    private void printArrayPositions(String[] arr){
+        System.out.println(arr.length); //length of the array
+        System.out.println(arr[0]);//first element
+        System.out.println(arr[1]);//second element
+        System.out.println(arr[arr.length-1]);//last element
+    }
 }
