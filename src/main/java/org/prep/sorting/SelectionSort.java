f202d2894dd24d5732168a5c192b37106532a5e4
Shisia <jamesshisia6@gmail.com>
2025-02-10 12:26:36 +0300

    no need to go to the end element because we are not comparing it to any element

--- src/main/java/org/prep/sorting/SelectionSort.java
+++ src/main/java/org/prep/sorting/SelectionSort.java
@@ -53,7 +53,7 @@ public class SelectionSort {
     }
 
     private void selectionSortV2(){
-        for(int i=0;i<array.length;i++){
+        for(int i=0;i<array.length-1;i++){
             int minIndex = i;
             long minValue = array[i];
             for(int j=i+1;j<array.length;j++){
